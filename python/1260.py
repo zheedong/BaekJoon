96acf9581c7d958ea1d453a25f2af8ca106ed31b
zheedong <zheedong@gmail.com>
2021-12-15 02:48:54 +0000

    1260 solved

--- python/1260.py
+++ python/1260.py
@@ -20,12 +20,28 @@ search_stack = deque([v])
 dfs_searched_node = []
 while search_stack:
     stack_pop = search_stack.pop()
-    print(search_stack)
-    if stack_pop in search_stack:
+    if stack_pop in dfs_searched_node:
         continue
     else:
         dfs_searched_node.append(stack_pop)
-        search_stack.append(sorted(list(edge[stack_pop])))
+        reversed_node = reversed(sorted(edge[stack_pop]))
+        for node in reversed_node:
+            search_stack.append(node)
         
-print(dfs_searched_node)
-    
+# BFS
+bfs_searched_node = []
+while search_queue:
+    queue_pop = search_queue.popleft()
+    if queue_pop in bfs_searched_node:
+        continue
+    else:
+        bfs_searched_node.append(queue_pop)
+        edges = sorted(edge[queue_pop])
+        for node in edges:
+            search_queue.append(node)
+        
+for i in dfs_searched_node:
+    print(i, end = " ")
+print()
+for j in bfs_searched_node:
+    print(j, end = " ")
