74c96b3b9416b6052b1b1b5637f3f89c6348442e
zheedong <zheedong@gmail.com>
2022-10-07 21:43:03 +0900

    2022-10-07 solved

--- python/17626.py
+++ python/17626.py
@@ -1,22 +1,24 @@
-# 일단 포기
-
 import math
-
 n = int(input())
 
-min_square_dict = dict([])
-min_square_dict[1] = 1    # 1^2
+# n을 k개의 제곱수의 합으로 나타낼 수 있는가?
+def is_n_sum_of_k_squares(n, k):
+    square_n = int(math.sqrt(n))
+    # Base Case k == 1
+    if k == 1:
+        if square_n ** 2 == n:
+            return True
+    elif k > 1:
+        flag = False
+        for cur_poss in range(square_n, 0, -1):
+            flag = flag | is_n_sum_of_k_squares(n - (cur_poss ** 2), k - 1)
+        return flag
+    return False
 
-def get_min_square(n):
-    gaus_sqrt_n = int(math.sqrt(n))
-    
-    if math.pow(gaus_sqrt_n, 2) == n:
-        min_square_dict[n] = 1
-        return
-    
-    min_square_dict[n] = n    # 1^2 + ... n번
-    for i in range(2, n):
-        if min_square_dict[n] > 
-        
-get_min_square(n)
-print(min_square_dict[n])
+k = 1
+while(True):
+    if is_n_sum_of_k_squares(n, k):
+        print(k)
+        break
+    else:
+        k += 1
