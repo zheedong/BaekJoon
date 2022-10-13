4c2352dc9a87a79a5329c154e4064e8686005702
zheedong <zheedong@gmail.com>
2022-10-13 14:34:39 +0900

    2022-10-13 solved

--- python/25083.py
+++ python/25083.py
@@ -1,9 +1,6 @@
-str = """
-         ,r'"7
-r`-_   ,'  ,/
- \. ". L_r'
-   `~\/
-      |
-      |
-"""
-print(str)
+print("         ,r\'\"7")
+print("r`-_   ,\'  ,/")
+print(" \\. \". L_r\'")
+print("   `~\\/")
+print("      |")
+print("      |")
