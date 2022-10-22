8f154171a6711821ba5f0201a0ad84928e29be14
zheedong <zheedong@gmail.com>
2022-10-22 11:41:07 +0900

    2022-10-22 solved

---
+++ python/2420.py
@@ -0,0 +1,3 @@
+import math
+n, m = map(int, input().split())
+print(abs(n - m))
---
+++ python/2667.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+apart_list = []
+
+for _ in range(n):
+    apart_list.append(list(map(int, input().split())))
+
+
