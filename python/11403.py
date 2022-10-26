b742e18bbdfd148833c4eada85144456b2808c66
zheedong <zheedong@gmail.com>
2022-10-26 11:44:38 +0900

    2022-10-26 solved

--- python/11403.py
+++ python/11403.py
@@ -1,8 +1,21 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-adj_list = []
+INF = 9999999999
+n = int(input().rstrip())
 
-for i in range(n):
-    adj_list.append(list(map(int, input().split())))
+adj_mat = [[None] * (n + 1) for _ in range(n + 1)]
+# 혼동을 피하기 위해 사용하지 않는 0열, 0행은 None
+for row in adj_mat[1:]:
+    row[1:] = list(map(lambda x : INF if x == 0 else 1, list(map(int, input().split()))))
 
-print(adj_list)
+# 플로이드-워셜 알고리즘
+# 시간 복잡도 O(V^3), 모든 노드에서 모든 노드의 거리를 구하는 상황.
+for mid in range(1, n + 1):
+    for start in range(1, n + 1):
+        for end in range(1, n + 1):
+            if adj_mat[start][end] > adj_mat[start][mid] + adj_mat[mid][end]:
+                adj_mat[start][end] = adj_mat[start][mid] + adj_mat[mid][end]
+
+for row in adj_mat[1:]:
+    print(*list(map(lambda x : 0 if x == INF else 1,row[1:])))
