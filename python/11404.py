273c0bf72d7e770674548a7be30fb68648d7b90e
zheedong <zheedong@gmail.com>
2022-11-21 14:46:35 +0900

    2022-11-21 solved

--- python/11404.py
+++ python/11404.py
@@ -16,7 +16,7 @@ for _ in range(m):
     min_dist[a][b] = min(min_dist[a][b], c)
 
 # Easy Floyd Warshall algorithm
-# n is small, so you don't need to worry about O(n^3)
+# n is small, so you don't need to worry about O(n^3) and memroy.
 for m in range(1, n + 1):
     for s in range(1, n + 1):
         for e in range(1, n + 1):
@@ -26,4 +26,4 @@ for m in range(1, n + 1):
                 min_dist[s][e] = min_dist[s][m] + min_dist[m][e]
 
 for row in min_dist[1:]:
-    print(*list(map(lambda x : 0 if x >= INF else x ,row[1:])))
+    print(*list(map(lambda x : 0 if x >= INF else x, row[1:])))
