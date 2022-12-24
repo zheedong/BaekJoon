1301293187f2618c1d9a981d896279d1df1a5283
zheedong <zheedong@gmail.com>
2022-12-24 14:37:54 +0900

    5525 not solved

--- python/5525.py
+++ python/5525.py
@@ -2,18 +2,19 @@ n = int(input())
 m = int(input())
 ioi_str = input()
 
-p_n = "I"
-for _ in range(n):
-    p_n += "OI"
+p_n = "I" 
+p_n += "OI" * n
 
-cnt = 0
+# O(NM)
+def naive_sol():
+    cnt = 0
+    for idx in range(m - len(p_n)):
+        if p_n == str(ioi_str[idx:idx + len(p_n)]):
+            cnt += 1
+    return cnt
 
-for idx in range(m - len(p_n)):
-    if ioi_str[idx] == "O":
-        continue
-    elif ioi_str[idx + 1] == "I":
-        continue
-    elif p_n == str(ioi_str[idx:idx + len(p_n)]):
-        cnt += 1
+pi_table = [-1, 0, 0, ]
 
-print(cnt)
+# O(N + M)
+def kmp():
+    print()
