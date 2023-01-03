e3bc7873750c3df12a7398313f4bc17183242a81
zheedong <zheedong@gmail.com>
2023-01-03 13:33:14 +0900

    2023-01-03 solved

---
+++ python/2581.py
@@ -0,0 +1,31 @@
+m = int(input())
+n = int(input())
+
+prime_list = []
+
+def is_prime(x):
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+for prime in range(2, n + 1):
+    if is_prime(prime):
+        prime_list.append(prime)
+
+print(prime_list)
+
+for idx, prime in enumerate(prime_list):
+    if prime >= m:
+        prime_list = prime_list[idx:]
+        break
+else:
+    prime_list = []
+
+print(prime_list)
+
+if not prime_list:
+    print(-1)
+else:
+    print(sum(prime_list))
+    print(prime_list[0])
