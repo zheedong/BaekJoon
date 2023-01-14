f331869b146e15a21d3a600a478c912f75f4e9c8
zheedong <zheedong@gmail.com>
2023-01-14 14:20:19 +0900

    2023-01-14 sovled

--- python/9252.py
+++ python/9252.py
@@ -1,5 +1,7 @@
-str_1 = list(input())
-str_2 = list(input())
+# https://velog.io/@emplam27/알고리즘-그림으로-알아보는-LCS-알고리즘-Longest-Common-Substring와-Longest-Common-Subsequence
+
+str_1 = [None] + list(input())
+str_2 = [None] + list(input())
 
 lcs = []
 for _ in range(len(str_1)):
@@ -7,15 +9,29 @@ for _ in range(len(str_1)):
 
 for i in range(len(str_1)):
     for j in range(len(str_2)):
-        # 같은 문자가 발견되었을 경우
-        # 참고해야 하는 str의 index의 주의. 0은 padding이라서 str이 1부터 시작한다고 생각함.
-        if str_1[i - 1] == str_2[j - 1]:
-            # 지금까지 가장 큰 LCS에서 1을 더한다.
+        if i == 0 or j == 0:
+            lcs[i][j] = 0
+        elif str_1[i] == str_2[j]:
             lcs[i][j] = lcs[i - 1][j - 1] + 1
-        # 없다면?
         else:
-            # LCS는 연속된 값이 아닐 수도 있다. 그렇기에 지금까지 LCS 값 중에서 가져와야 한다.
-            # 예시 : LCS(AB, GBC) = max(LCS(A, GBC), LCS(AB, GB))
             lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1])
-# LCS string을 만드는 알고리즘도 있지만, 길이를 물어 봐서 가장 마지막 값이 LCS의 길이다.
+
 print(lcs[i][j])
+
+i = len(str_1) - 1
+j = len(str_2) - 1
+result = []
+while True:
+    if lcs[i][j] == 0:
+        break
+    elif lcs[i][j] == lcs[i - 1][j]:
+        i -= 1
+    elif lcs[i][j] == lcs[i][j - 1]:
+        j -= 1
+    else:
+        result.append(str_1[i])
+        i -= 1
+        j -= 1
+result.reverse()
+
+print("".join(result))
